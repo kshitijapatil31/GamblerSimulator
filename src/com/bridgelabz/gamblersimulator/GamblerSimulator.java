e5a33e8319aeb141384d7211df04578f0abb8953
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-12 13:12:18 +0530

    [kshitija]: usecase2 is solved

--- src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -1,10 +1,23 @@
 package com.bridgelabz.gamblersimulator;
 
+import java.util.Random;
+
 public class GamblerSimulator {
 
+	public static int winOrLoose() {
+        Random ran=new Random();
+          int num=ran.nextInt();
+          return num;
+
+	}
 	public static void main(String[] args) {
 		final int stake=100;
 		final int bets=1;
+		if(winOrLoose()==1) {
+			System.out.println("gambler is win the game");
+		}else {
+			System.out.println("gambler is loose the game");
+		}
 
 	}
 
