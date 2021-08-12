14da7a447012a14224d9a17c472b18290d81cf54
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-12 22:06:23 +0530

    [kshitija]:usecase 3 solved

--- src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -3,22 +3,42 @@ package com.bridgelabz.gamblersimulator;
 import java.util.Random;
 
 public class GamblerSimulator {
+	static int stake = 100;
+	static final int bets = 1;
+	static int flag = 0;
 
-	public static int winOrLoose() {
-        Random ran=new Random();
-          int num=ran.nextInt();
-          return num;
+	public static void winOrLoose() {
+		Random ran = new Random();
+		int num = ran.nextInt(2);
 
-	}
-	public static void main(String[] args) {
-		final int stake=100;
-		final int bets=1;
-		if(winOrLoose()==1) {
+		if (num == 1) {
 			System.out.println("gambler is win the game");
-		}else {
+			stake++;
+		} else {
 			System.out.println("gambler is loose the game");
+			stake--;
 		}
 
+	}
+
+	public static void cumulativeStake() {
+		int winningAmount = (int) ((int) stake + (0.5 * stake));
+		int loosingAmount = (int) ((int) stake - (0.5 * stake));
+		// int winninCash=Math.random();
+		winOrLoose();
+		while(flag==0) {
+		if (stake == winningAmount) {
+          flag++;
+		}
+		if (stake == loosingAmount) {
+            flag++;
+		}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		cumulativeStake();
 	}
 
 }
